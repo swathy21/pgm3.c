5b7d3bb1ac7e3fb2c65a6c4087f5b87846246398
swathy21 <42170034+swathy21@users.noreply.github.com>
2019-02-06 06:45:28 -0800

    Create pgm3.c

---
+++ pgm3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')!!(a='A'||a=='E'||a=='I'||a=='O'||a=='U')
+{
+printf("vowel");
+else
+printf("consanant")
+}
+getch();
+}
